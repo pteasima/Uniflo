0357f177d57a0b656da7c45f7d9e21291661dfdc
Petr Sima <ptea.sima@gmail.com>
2019-08-17 00:03:11 +0200

    added overload to SubscriptionEffect.init which enables the use of Action's enum properties for the output transform if the Action is EmptyInitializable

--- Sources/Uniflo/SubscriptionEffect.swift
+++ Sources/Uniflo/SubscriptionEffect.swift
@@ -12,9 +12,42 @@ public struct SubscriptionEffect<Action, Environment>: Equatable {
         data = (keyPath, input, transform)
         eq = { other in keyPath == other.data.keyPath && input == other.data.input as? Input && transform == other.data.transform }
     }
+    public init<Output, P: Publisher>(_ keyPath: KeyPath<Environment, () -> P>, _ transform: KeyPath<Output, Action>) where P.Output == Output, P.Failure == Never {
+        perform = { effects in
+            effects[keyPath: keyPath]().map { $0[keyPath: transform] }.eraseToAnyPublisher()
+        }
+        data = (keyPath, (), transform)
+        eq = { other in keyPath == other.data.keyPath && transform == other.data.transform }
+    }
     
     public static func ==(lhs: SubscriptionEffect, rhs: SubscriptionEffect) -> Bool {
         lhs.eq(rhs)
     }
 }
 
+// above we use `transform` to produce Action from Output. But if Action is EmptyInitilizable, we can create the action ourselves, and `inject` the value into it.
+// if action accepts Output as its only parameter (most of the time), we can use Action's enum properties (no need to extend Output with a var)
+public extension SubscriptionEffect where Action: EmptyInitializable {
+  init<Input: Equatable, Output, P: Publisher>(_ keyPath: KeyPath<Environment, (Input) -> P>, _ input: Input, _ inject: WritableKeyPath<Action, Output?>) where P.Output == Output, P.Failure == Never {
+      perform = { effects in
+          effects[keyPath: keyPath](input).map {
+            var action = Action()
+            action[keyPath: inject] = $0
+            return action
+          }.eraseToAnyPublisher()
+      }
+      data = (keyPath, input, inject)
+      eq = { other in keyPath == other.data.keyPath && input == other.data.input as? Input && inject == other.data.transform }
+  }
+  init<Output, P: Publisher>(_ keyPath: KeyPath<Environment, () -> P>, _ inject: WritableKeyPath<Action, Output?>) where P.Output == Output, P.Failure == Never {
+      perform = { effects in
+          effects[keyPath: keyPath]().map {
+            var action = Action()
+            action[keyPath: inject] = $0
+            return action
+          }.eraseToAnyPublisher()
+      }
+      data = (keyPath, (), inject)
+      eq = { other in keyPath == other.data.keyPath && inject == other.data.transform }
+  }
+}
